1b23a38233d0f7b6e2462b7cc100e01868e9cac6
Tristan Lafont Rapnouil <you@example.com>
2022-04-20 14:09:04 -0300

    comment pascotransfo2

--- R/PASCO_transfo2.R
+++ R/PASCO_transfo2.R
@@ -42,46 +42,45 @@ PASCO_transfo2 <- function(data,
 
 
 
+#RENAME
+  col_names <- rbind(paste0("Date_Time_",name_run),paste0("Time_s_",name_run)) #create a vector containing date and time columns names
 
-  col_names <- rbind(paste0("Date_Time_",name_run),paste0("Time_s_",name_run))
-
-  for (i in 1:length(col_names)){
-
-    if (i<=2){
+  for (i in 1:length(col_names)){ #for each names
+    if (i<=2){ # for the two first column just get the two first names
       colnames(data_pasco)[i] <- col_names[i]
     }
-    else{
+    else{ # for any other columns, skip a number of columns corresponding to the number of probes/samples
       colnames(data_pasco)[i+(ceiling(i/2)-1)*nb_ech] <- col_names[i]
     }
   }
 
-  co2_columns <- which(grepl("Concentration",colnames(data_pasco)))
-  col_to_change<-split(co2_columns,sort(c(1:length(co2_columns))%%n_run))
-  for (i in 1:length(col_to_change)){
-    colnames(data_pasco)[col_to_change[[i]]] <- paste0("CO2_ppm_",ech,"_",name_run[i])
+  co2_columns <- which(grepl("Concentration",colnames(data_pasco))) #get Co2 columns position (contains "Concentration")
+  col_to_change<-split(co2_columns,sort(c(1:length(co2_columns))%%n_run)) #split them in n_run equalsized groups
+
+  for (i in 1:length(col_to_change)){ # for each Co2  columns
+    colnames(data_pasco)[col_to_change[[i]]] <- paste0("CO2_ppm_",ech,"_",name_run[i]) #rename them
   }
 
-  New_Col_Names <- paste0("Hour_time_",name_run)
-  position <- which(grepl("Date_Time_",colnames(data_pasco)))
+  New_Col_Names <- paste0("Hour_time_",name_run) #create names for columns time in hour
+  position <- which(grepl("Date_Time_",colnames(data_pasco))) #get position of date_time columns
+  extra_col <- 1:nrow(data_pasco) # create a column to be completed
 
-
-  extra_col <- 1:nrow(data_pasco)
-  for (i in position){
-    tempo <- ifelse(data[[i]]!="",stringr::str_extract(data[[i]],"([0-1]?\\d|2[0-3]):([0-5]?\\d):([0-5]?\\d)"),NA)
-    extra_col <- data.frame(extra_col,tempo)
+  for (i in position){ #for each date_time column
+    tempo <- ifelse(data[[i]]!="",stringr::str_extract(data[[i]],"([0-1]?\\d|2[0-3]):([0-5]?\\d):([0-5]?\\d)"),NA) #extract time
+    extra_col <- data.frame(extra_col,tempo) #append extracol
   }
 
-  names(extra_col) <- c("x",New_Col_Names)
-  data_pasco <- cbind(data_pasco,extra_col[,2:length(extra_col)])
+  names(extra_col) <- c("x",New_Col_Names) #rename these new col
+  data_pasco <- cbind(data_pasco,extra_col[,2:length(extra_col)]) #add them to data_pasco, rming useless "x" col
 
   data_pasco <- dplyr::mutate(.data=data_pasco,
-                              Date= stringr::str_sub(data_pasco[[1]],1,10))
+                              Date= stringr::str_sub(data_pasco[[1]],1,10)) #create a date only column
 
-  position <- which(grepl("Time_s",colnames(data_pasco)))
+  position <- which(grepl("Time_s",colnames(data_pasco))) #get position of time in second columns
   data_pasco <- dplyr::mutate(.data=data_pasco,
                               dplyr::across(.cols = position,.fns = as.numeric))%>%
                 dplyr::mutate(.data=data_pasco,
-                              dplyr::across(.cols = position,.fns = ~.x*(1/3600),.names = "hour_{.col}"))
+                              dplyr::across(.cols = position,.fns = ~.x*(1/3600),.names = "hour_{.col}")) #create hour columns
 
   #get all CO2 measurements
   CO2_ppm <- NULL
@@ -106,6 +105,7 @@ PASCO_transfo2 <- function(data,
     length_vec <- c(length_vec,a)
   }
 
+  #for each run, cut it to the shortest co2 probe measurements
   CO2_ppm<-split(CO2_ppm,sort(c(1:length(CO2_length))%%n_run))
 
   for (i in 1:length(CO2_ppm)){
@@ -116,6 +116,7 @@ PASCO_transfo2 <- function(data,
 
   }
 
+  #repeat each ID
   Ech_ID <- list()
   for (i in 1:n_run){
     a <- NULL
@@ -125,6 +126,8 @@ PASCO_transfo2 <- function(data,
     Ech_ID[[i]] <- a
   }
 
+
+  #repeat time
   Time_hms <- list()
   position <- which(grepl("Hour_time_",colnames(data_pasco)))
   for (i in 1:n_run){
@@ -133,6 +136,7 @@ PASCO_transfo2 <- function(data,
     Time_hms[[i]] <- a
   }
 
+  #repeat time "2"
   Time_h <- list()
   position <- which(grepl("hour_Time_s",colnames(data_pasco)))
   for (i in 1:n_run){
@@ -141,6 +145,7 @@ PASCO_transfo2 <- function(data,
     Time_h[[i]] <- a
   }
 
+  #repeat flux type
   Flux_type <- list()
   for (i in 1:n_run){
     a <- unlist(rep(rep(name_run[i],length_vec[i]),nb_ech))
@@ -148,6 +153,7 @@ PASCO_transfo2 <- function(data,
     Flux_type[[i]] <- a
   }
 
+  # create gasfluxes compatible table
   gasfluxes_data_pasco <- tibble::tibble(
     CO2_ppm = unlist(CO2_ppm),
     Time = hms::as_hms(unlist(Time_hms)),
@@ -156,6 +162,7 @@ PASCO_transfo2 <- function(data,
     Ech_ID = unlist(Ech_ID)
   )
 
+  # mutate to add new columns
   gasfluxes_data_pasco <- dplyr::mutate(.data=gasfluxes_data_pasco,
                                         Rec_ID=paste0(Ech_ID,"_",Flux_type),
                                         CO2_mg_m3=((CO2_ppm/1000)*44.01)/24.45,
@@ -164,6 +171,7 @@ PASCO_transfo2 <- function(data,
                                         V=V
   )
 
+  # arrange columns position
   gasfluxes_data_pasco <- dplyr::relocate(.data=gasfluxes_data_pasco,
                                           Date,
                                           Time,
@@ -175,13 +183,6 @@ PASCO_transfo2 <- function(data,
                                           CO2_mg_m3
   )
 
-  # proby <- NULL
-  # for (i in 1:nrow(gasfluxes_data_pasco)){
-  #   proby_temp <- match(gasfluxes_data_pasco$Ech_ID[i],ech)
-  #   proby <- c(proby,proby_temp)
-  # }
-  #
-  # gasfluxes_data_pasco <- dplyr::mutate(gasfluxes_data_pasco,Probe=proby )
   return(gasfluxes_data_pasco)
 
 }
