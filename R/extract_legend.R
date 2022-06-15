b8d96bf99ac6293b5f0cd1ce06f2d833e45ac6dd
Tristan Lafont Rapnouil <you@example.com>
2022-06-15 09:50:12 -0300

    rename function!

--- R/extract_legend.R
+++ R/extract_legend.R
@@ -1,11 +1,11 @@
-#' Title
+#' xtract_legend
 #'
 #' @param my_ggp a ggplot object to which you want to get the legend
 #'
 #' @return a legend object that can be added to a ggplot
 #' @export
 #' @import ggplot2
-extract_legend <- function(my_ggp) {
+xtract_legend <- function(my_ggp) {
   step1 <- ggplot_gtable(ggplot_build(my_ggp))
   step2 <-
     which(sapply(step1$grobs, function(x)
