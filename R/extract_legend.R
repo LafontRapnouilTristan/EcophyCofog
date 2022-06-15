805cd266880017438921b5b584f5cc0d5d0f6643
Tristan Lafont Rapnouil <you@example.com>
2022-06-15 09:21:10 -0300

    add extractlegend

---
+++ R/extract_legend.R
@@ -0,0 +1,15 @@
+#' Title
+#'
+#' @param my_ggp a ggplot object to which you want to get the legend
+#'
+#' @return a legend object that can be added to a ggplot
+#' @export
+#' @import ggplot2
+extract_legend <- function(my_ggp) {
+  step1 <- ggplot_gtable(ggplot_build(my_ggp))
+  step2 <-
+    which(sapply(step1$grobs, function(x)
+      x$name) == "guide-box")
+  step3 <- step1$grobs[[step2]]
+  return(step3)
+}
