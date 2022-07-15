4d8d52df97ef1cb5c4b9cbdfb0af1f3d34b362f3
Tristan Lafont Rapnouil <you@example.com>
2022-07-15 08:23:42 -0300

    Add Library fctn: install and load multiple pckg

---
+++ R/Library.R
@@ -0,0 +1,12 @@
+#' Library
+#'
+#' @param Packages character vector with ur packages to install or load
+#' @importFrom utils install.packages installed.packages
+#' @export
+Library <- function(Packages) {
+  InstallAndLoad <- function(Package) {
+    if (!Package %in% installed.packages()[, 1]) {install.packages(Package, repos="https://cran.rstudio.com/")}
+    require(Package, character.only = TRUE)
+  }
+  invisible(sapply(Packages, InstallAndLoad))
+}
